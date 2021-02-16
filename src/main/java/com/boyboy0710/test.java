1ce4a59438eaa047ae8e780a72a6cb95a8a777c3
boyboy0710 <boyboy0710@naver.com>
2021-02-16 21:25:07 +0900

    Initial commit

---
+++ src/main/java/com/boyboy0710/test.java
@@ -0,0 +1,39 @@
+package com.boyboy0710;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.block.Action;
+
+public final class test extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onInteract(PlayerInteractEvent e) {
+        Player p = e.getPlayer();
+        if (e.getAction() == Action.RIGHT_CLICK_AIR
+                || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            if(e.getItem().getType() == Material.TNT) {
+                Entity entity =
+                        p.getWorld().spawnEntity(p.getEyeLocation(), EntityType.PRIMED_TNT);
+                entity.setVelocity(p.getLocation().getDirection().multiply(2));
+            }
+        }
+    }
+
+}
