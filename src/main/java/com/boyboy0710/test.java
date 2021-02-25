1bde9cb5eb60c309b8c51572c9e71cb64981d6d1
boyboy0710 <boyboy0710@naver.com>
2021-02-25 12:17:41 +0900

    손안에 들고있는 아이템 발사추가

--- src/main/java/com/boyboy0710/test.java
+++ src/main/java/com/boyboy0710/test.java
@@ -1,24 +1,51 @@
 package com.boyboy0710;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.block.Action;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public final class test extends JavaPlugin implements Listener {
 
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        getServer().getScheduler().scheduleSyncRepeatingTask(this, () -> {
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                List<ArmorStand> stands = new ArrayList<>();
+                List<Entity> entities = Bukkit.getWorld("world").getEntities();
+                for (Entity entity : entities) {
+                    if (entity.getType().equals(EntityType.ARMOR_STAND)) {
+                        stands.add((ArmorStand) entity);
+                    }
+                }
+                for (ArmorStand armorStand : stands) {
+                    if (armorStand.getScoreboardTags().contains(player.getName())) {
+                        armorStand.teleport(armorStand.getLocation().add(armorStand.getLocation().getDirection().multiply(1)));
+                    }
+                    for (Entity entity : entities) {
+                        if (entity.getLocation().distance(armorStand.getLocation()) < 1) {
+                            ((org.bukkit.entity.LivingEntity) entity).damage(5);
+                        }
+                    }
+                }
+            }
+        }, 0, 0);
     }
+
 
     @Override
     public void onDisable() {
@@ -34,6 +61,13 @@ public final class test extends JavaPlugin implements Listener {
                 Entity entity =
                         p.getWorld().spawnEntity(p.getEyeLocation(), EntityType.PRIMED_TNT);
                 entity.setVelocity(p.getLocation().getDirection().multiply(2));
+            }
+            else {
+                World world = e.getPlayer().getWorld();
+                ArmorStand stand = world.spawn(p.getLocation(), ArmorStand.class);
+                stand.addScoreboardTag(p.getName());
+                stand.setItemInHand(p.getInventory().getItemInMainHand());
+                p.getInventory().setItemInMainHand(null);
             }
         }
     }
